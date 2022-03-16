0401e02e476f00ff42ea967e0fa836037b23cfb5
Yoha Axed <yohaxed747@siberpay.com>
2022-03-16 15:16:42 +0000

    FroontEnd component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import User from "./component/user/User";
 
 function App() {
-  return <><h1>Hello</h1></>;
+  return (
+    <>
+      <h1>Hello</h1> <User />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/component/user/User.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import "./User.css"
+
+const User = () => {
+  return (
+    <>
+      <h2>User Works</h2>
+    </>
+  );
+};
+
+export default User;
