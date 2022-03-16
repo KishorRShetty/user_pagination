6d470ee7ff33801cca2276e240789538be7388dd
Yoha Axed <yohaxed747@siberpay.com>
2022-03-16 11:51:26 +0000

    get all users  done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,36 @@
 const userModel = require("../models/userModel");
 
 exports.registerUser = async (req, res) => {
-  try{
-  const user = await userModel.create(req.body);
-  // we can also use const us = new userModel(user) and await us.save()
-  //get data from the body
-  //I think we can also send createdAt from here. But here it's included in the model.
-  // passing the user and email from here
-  //201 created
-  res.status(201).json({
-    success: true,
-    user,
-  });
-}catch(error){
-  res.status(400).json({
-    success: false,
-    message: "data Error"
-  })
-}
+  try {
+    const user = await userModel.create(req.body);
+    // we can also use const us = new userModel(user) and await us.save()
+    //get data from the body
+    //I think we can also send createdAt from here. But here it's included in the model.
+    // passing the user and email from here
+    //201 created
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "data Error",
+    });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const user = await userModel.find({});
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "data Error",
+    });
+  }
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userController");
+const { registerUser, getUsers } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
+router.route("/allUsers").get(getUsers);
 
 module.exports = router;
