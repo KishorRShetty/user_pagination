ff7c1a2455e86f4930d1771970820d12cff3cfd2
Yoha Axed <yohaxed747@siberpay.com>
2022-03-16 08:53:52 +0000

    mongo connection done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require("./config/database");
+
 
 dotenv.config({ path: "backend/config/config.env" });
+//after env db for getting the path
 
+connectDatabase();
 
 
 const server = app.listen(process.env.PORT, () => {
