1ea015219f31b1465fe861e470dc646d1b0e2ede
Yoha Axed <yohaxed747@siberpay.com>
2022-03-21 10:07:41 +0000

    snackbar_green

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
-echo "Commit info: "
+echo "Commit info without spaces: "
 read x
 git add . && git commit -m ${x} && git push
 
 
 # single line failed :>>
-#read -p "Commit info: " x
+#read -p echo "Commit info without spaces: " x
 #git add . && git commit -m ${x} && git push
